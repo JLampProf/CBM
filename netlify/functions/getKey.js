a33c174349fa1f7f2dcb4aecb51cee1a54bfbf72
JCLamprecht <66572608+JCLamprecht@users.noreply.github.com>
2025-04-10 15:51:02 +0200

    getKEy change

--- netlify/functions/getKey.js
+++ netlify/functions/getKey.js
@@ -2,14 +2,7 @@ require("dotenv").config();
 
 exports.handler = async (event, context) => {
   const key = process.env.GOOGLE_MAPS_API_KEY;
-};
 
-const app = express();
-const PORT = process.env.PORT || 8000;
-
-app.use(cors());
-
-app.get("/api/key", async (req, res) => {
   if (!key) {
     return {
       statusCode: 300,
@@ -21,4 +14,4 @@ app.get("/api/key", async (req, res) => {
     statusCode: 200,
     body: JSON.stringify({ key }),
   };
-});
+};
